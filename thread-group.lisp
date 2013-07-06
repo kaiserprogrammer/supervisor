54bbdb184eeef7db3cfa9a2462ec90d5b3414a47
Jürgen Bickert <juergenbickert@gmail.com>
2013-07-06 02:03:55 +0200

    make thread-group itself thread-safe

--- thread-group.lisp
+++ thread-group.lisp
@@ -22,7 +22,9 @@
             :accessor checker)
    (name :initarg :name
          :initform (princ-to-string (gensym "ThreadGroup"))
-         :accessor name)))
+         :accessor name)
+   (lock :accessor lock
+         :initform (bt:make-lock))))
 
 (defun start (&key (runner *runner*) (check-sleep 5))
   (when (and (not (and (checker runner)
@@ -33,20 +35,22 @@
            (lambda ()
              (unwind-protect
                   (loop
-                     (dolist (task (tasks runner))
-                       (let* ((name (car task))
-                              (function (cdr task))
-                              (thr (find name (threads runner) :key #'car :test #'equalp)))
-                         (when (not (and (cdr thr) (bt:thread-alive-p (cdr thr))))
-                           (stop-thread runner name)
-                           (setf (threads runner) (remove name (threads runner) :key #'car :test #'equalp))
-                           (push (cons name (bt:make-thread
-                                             (lambda ()
-                                               (let ((*runner* runner))
-                                                 (funcall function))) :name (concatenate 'string (name runner) name)))
-                                 (threads runner)))))
+                     (bt:with-lock-held ((lock runner))
+                       (dolist (task (tasks runner))
+                         (let* ((name (car task))
+                                (function (cdr task))
+                                (thr (find name (threads runner) :key #'car :test #'equalp)))
+                           (when (not (and (cdr thr) (bt:thread-alive-p (cdr thr))))
+                             (stop-thread runner name)
+                             (setf (threads runner) (remove name (threads runner) :key #'car :test #'equalp))
+                             (push (cons name (bt:make-thread
+                                               (lambda ()
+                                                 (let ((*runner* runner))
+                                                   (funcall function))) :name (concatenate 'string (name runner) ":" name)))
+                                   (threads runner))))))
                      (sleep check-sleep))
                (when (checker runner)
+                 (setf (checker runner) nil)
                  (stop :runner runner))))
            :name (concatenate 'string (name runner) ":checker")))))
 
@@ -55,20 +59,32 @@
     (bt:join-thread (checker runner))))
 
 (defun stop (&key (runner *runner*))
+  (when (checker runner)
+    (bt:with-lock-held ((lock runner))
+      (let ((thr (checker runner)))
+        (setf (checker runner) nil)
+        (when (bt:thread-alive-p thr)
+          (ignore-errors (bt:destroy-thread thr))))))
   (let ((threads (mapcar #'cdr (threads runner))))
-    (when (checker runner)
-      (push (checker runner) threads))
-    (setf (checker runner) nil)
-    (let ((thr (bt:current-thread)))
-      (when (member thr threads :test #'eq)
-        (setf threads (append (remove (bt:current-thread) threads :test #'eq) (list thr)))))
-    (dolist (thr threads)
-      (when (bt:thread-alive-p thr)
-        (ignore-errors (sb-thread:terminate-thread thr))))))
+    (let ((thr (find (bt:current-thread) threads :test #'eq)))
+      (when thr
+        (setf threads (remove thr threads :test #'eq)))
+      (dolist (thr threads)
+        (when (bt:thread-alive-p thr)
+          (ignore-errors (bt:destroy-thread thr))))
+      (when thr
+        (handler-bind
+            ((BORDEAUX-THREADS::BORDEAUX-MP-CONDITION
+              (lambda (c)
+                (declare (ignore c))
+                (let ((restart (find-restart 'abort)))
+                  (when restart
+                    (invoke-restart restart))))))
+          (bt:destroy-thread thr))))))
 
 (defun add-lambda (function &key (runner *runner*)
                          (name (princ-to-string
-                                (gensym (princ-to-string (name runner))))))
+                                (gensym))))
   (push (cons name function) (tasks runner)))
 
 (defun remove-lambda (name &key (runner *runner*))
@@ -79,7 +95,7 @@
 (defmethod stop-thread ((group thread-group) name)
   (let ((thr (find name (threads group) :key #'car :test #'equalp)))
     (when (and (cdr thr) (bt:thread-alive-p (cdr thr)))
-      (sb-thread:terminate-thread (cdr thr)))))
+      (bt:destroy-thread (cdr thr)))))
 
 (defmethod thread-start ((group thread-group)))
 
@@ -97,6 +113,16 @@
   (with-setup
     (start)
     (assert-false (join))))
+
+(define-test fast-stop
+  (let ((*runner* (make-instance 'thread-group))
+        (thread-count (length (bt:all-threads)))
+        (run nil))
+    (add-lambda (lambda () (setf run t) (stop)))
+    (start)
+    (sleep 0.001)
+    (assert-true run)
+    (assert-eql thread-count (length (bt:all-threads)))))
 
 (define-test dont-run-when-not-started
   (with-setup
@@ -188,12 +214,19 @@
 (define-test stop-immediately-after-start
   (with-setup
     (let ((arun nil)
-          (brun nil))
-      (add-lambda (lambda () (sleep 0.09) (setf arun t)))
-      (add-lambda (lambda () (stop)))
-      (add-lambda (lambda () (sleep 0.09) (setf brun t)))
+          (brun nil)
+          (crun nil))
+      (add-lambda (lambda () (sleep 0.01) (setf arun t) (loop (sleep 1))) :name "Arunner")
+      (add-lambda (lambda () (progn
+                     (setf crun t)
+                     (stop)
+                     (sleep 0.01)
+                     (setf crun nil)))
+             :name "Crunner")
+      (add-lambda (lambda () (sleep 0.01) (setf brun t)) :name "Brunner")
       (start)
       (sleep 0.1)
+      (assert-true crun)
       (assert-false arun)
       (assert-false brun))))
 
@@ -209,7 +242,7 @@
       (add-lambda (lambda () (loop (sleep 1))))
       (start)
       (sleep 0.001)
-      (sb-thread:terminate-thread (checker *runner*))
+      (bt:destroy-thread (checker *runner*))
       (sleep 0.001)
       (assert-eql thread-count (length (bt:all-threads))))))
 
@@ -228,8 +261,9 @@
   (with-setup
     (let ((run nil)
           (cleanup nil))
-      (add-lambda (lambda () (unwind-protect (progn (stop) (setf run t))
-                     (setf cleanup t))))
+      (add-lambda (lambda () (sleep 0.0001)
+                (unwind-protect (progn (stop) (setf run t))
+                  (setf cleanup t))))
       (start)
       (sleep 0.01)
       (assert-false run)
